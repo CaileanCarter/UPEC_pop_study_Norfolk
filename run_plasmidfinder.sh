d56cadd561af77272c6fdcdd6df1a197e956735b
CaileanCarter <cailean.carter@quadram.ac.uk>
2023-03-17 15:59:51 +0000

    removed unneccesary scripts

--- run_plasmidfinder.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-for fasta in FASTA/*.fasta; do
-    ID=$(echo $fasta | cut -d '.' -f 1 | cut -d '/' -f 2)
-    # echo $ID
-    mkdir "PFOut/${ID}"
-    python3 plasmidfinder.py -i $fasta -o "PFOut/${ID}" -p plasmidfinder_db
-done
